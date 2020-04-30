d8b366fc45ad7f6faab36fd09c163219d7cb795a
Carmina <cvdsoto@gmail.com>
2020-04-30 18:48:00 +1000

    changed home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import './css/main.css';
 
 class Home extends Component {
@@ -6,7 +7,20 @@ class Home extends Component {
     return (
       <div>
         <main>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut justo vitae nulla eleifend consequat et at ante. Integer nec efficitur erat. Aliquam nec tempus dolor. Curabitur id turpis id ligula efficitur scelerisque. Maecenas vel ex ac purus placerat cursus.</p>
+          <h2>How does this work?</h2>
+          <div className="steps">
+            <span className="number">1</span>
+            <p>Type in the ingredients you have in your fridge</p>
+          </div>
+          <div className="steps">
+            <span className="number">2</span>
+            <p>Select dietary and health filters</p>
+          </div>
+          <div className="steps">
+            <span className="number">3</span>
+            <p>Choose the recipe that you want to try!</p>
+          </div>
+          <Link to ='/search'>Start your Search!</Link>
           <img src="https://truffle-assets.imgix.net/96f876c0-fallback-food-video-asset.jpg?auto=compress,format&fm=pjpg&w=1200" alt="food at table"/>
         </main>
       </div>
